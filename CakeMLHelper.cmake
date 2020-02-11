c0169a1cb1e345572eccc5546f9cd67640fff41a
Grant Jurgensen <gajurgensen@gmail.com>
2020-02-11 12:17:04 -0600

    Improve CakeML helper functions portability

--- CakeMLHelper.cmake
+++ CakeMLHelper.cmake
@@ -1,13 +1,13 @@
 # Config variables
 find_program(cake64 cake64)
 find_program(cake32 cake32)
-set(cake_flags "--stack_size=50 --heap_size=50" CACHE STRING "Arguments passed to the CakeML compiler")
+set(cake_flags "--stack_size=10 --heap_size=10" CACHE STRING "Arguments passed to the CakeML compiler")
 string(REGEX REPLACE "[ \t\r\n]+" ";" cakeflag_list "${cake_flags}")
 
-# Creates a target CakeML library, which you can use in a CAmkES component
-# Args: name - name used for file
+# Compile a list of CakeML source files. Creates a build target for library ${name}
+# Args: name - name of the resulting library
 #       SOURCES - CakeML source files, in order (they'll be concatenated together)
-#       ENTRY - Name of assembly symbol denoting start of the CakeML code. Defaults to "cml_entry"
+#       ENTRY_NAME - Name of assembly symbol denoting start of the CakeML code. Defaults to "run" (CAmkES component entry point)
 function(build_cake name)
     cmake_parse_arguments(
         PARSE_ARGV
@@ -27,7 +27,7 @@ function(build_cake name)
         set(PARSED_ARGS_ENTRY_NAME "run")
     endif()
 
-    if("${KernelSel4Arch}" STREQUAL "aarch32")
+    if("${KernelSel4Arch}" STREQUAL "aarch32" OR "${KernelSel4Arch}" STREQUAL "arm_hyp")
         if("${cake32}" STREQUAL "cake32-NOTFOUND")
             message(FATAL_ERROR "Could not find a 32-bit targeting CakeML compiler. Please ensure cake32 is on the system path.")
         endif()
@@ -41,8 +41,8 @@ function(build_cake name)
         message(FATAL_ERROR "No CakeML compiler support for architecture ${KernelSel4Arch}")
     endif()
 
-    cat(${name}.cml ${PARSED_ARGS_SOURCES})
-    set(abs_bin_prefix "${CMAKE_CURRENT_BINARY_DIR}/${name}")
+    cat("${name}.cml" ${PARSED_ARGS_SOURCES})
+    set(abs_bin_prefix "${CMAKE_BINARY_DIR}/${name}")
     add_custom_command(
         OUTPUT ${abs_bin_prefix}.cake.S
         COMMAND ${cake} ${cakeflag_list} < ${abs_bin_prefix}.cml > ${abs_bin_prefix}.cake.S
@@ -50,45 +50,43 @@ function(build_cake name)
         DEPENDS ${abs_bin_prefix}.cml
         VERBATIM
     )
-    set_source_files_properties(${abs_bin_prefix}.cake.S PROPERTIES GENERATED TRUE)
-    # add_library(${name} OBJECT ${abs_bin_prefix}.cake.S)
+    add_library(${name} STATIC "${abs_bin_prefix}.cake.S")
 endfunction()
 
-# Builds cake binary and declares a camkes component simultaneously, with default settings.
-# If finer-grained control is necessary, user should use build_cake and DeclareCAmkESComponent
-# instead.
+# Builds a CAmkES component from CakeML and C sourse files.
 # Args: name - name of the component
 #       CML_SOURCES - CakeML source files, in order (they'll be concatenated together)
 #       C_SOURCES - C source files
-#       ENTRY_NAME - Name of assembly symbol denoting start of the CakeML code. Defaults to "cml_entry"
-function(simple_cakeml_component name)
+#       LIBS - Libraries to link. Defaults to "camkescakeml"
+#       ENTRY_NAME - Name of assembly symbol denoting start of the CakeML code. Defaults to "run" (CAmkES component entry point)
+function(cakeml_component name)
     cmake_parse_arguments(
         PARSE_ARGV
         1
         PARSED_ARGS
         ""
-        "CML_SOURCES;C_SOURCES"
+        "CML_SOURCES;C_SOURCES;LIBS"
         "ENTRY_NAME"
     )
     if(NOT "${PARSED_ARGS_UNPARSED_ARGUMENTS}" STREQUAL "")
-        message(FATAL_ERROR "Unknown arguments to declare_cakeml_component ${PARSED_ARGS_UNPARSED_ARGUMENTS}")
+        message(FATAL_ERROR "Unknown arguments to cakeml_component ${PARSED_ARGS_UNPARSED_ARGUMENTS}")
+    endif()
+    if("${PARSED_ARGS_LIBS}" STREQUAL "")
+        set(PARSED_ARGS_LIBS "camkescakeml")
     endif()
 
-    build_cake(${name} SOURCES ${PARSED_ARGS_CML_SOURCES} ENTRY_NAME ${PARSED_ARGS_ENTRY_NAME})
-
-    set(abs_bin_prefix "${CMAKE_CURRENT_BINARY_DIR}/${name}")
+    build_cake("${name}.cake" SOURCES "${PARSED_ARGS_CML_SOURCES}" ENTRY_NAME "${PARSED_ARGS_ENTRY_NAME}")
     DeclareCAmkESComponent(${name}
-        SOURCES ${PARSED_ARGS_C_SOURCES} "${abs_bin_prefix}.cake.S"
-        LIBS camkescakeml
+        SOURCES "${PARSED_ARGS_C_SOURCES}"
+        LIBS "${PARSED_ARGS_LIBS}" "${name}.cake"
     )
 endfunction()
 
 # Concatenates files with unix "cat" program
 function(cat name file)
-    set(abs_name "${CMAKE_CURRENT_BINARY_DIR}/${name}")
+    set(abs_name "${CMAKE_BINARY_DIR}/${name}")
     foreach(filepath ${file} ${ARGN})
-        # Not sure why LIST_DIR works here instead of SOURCE_DIR
-        list(APPEND abs_files "${CMAKE_CURRENT_LIST_DIR}/${filepath}")
+        list(APPEND abs_files "${CMAKE_CURRENT_SOURCE_DIR}/${filepath}")
     endforeach(filepath)
 
     add_custom_command(
@@ -96,5 +94,4 @@ function(cat name file)
         COMMAND cat ${abs_files} > ${abs_name}
         DEPENDS ${abs_files}
     )
-    set_source_files_properties(${abs_name} PROPERTIES GENERATED TRUE)
 endfunction()
